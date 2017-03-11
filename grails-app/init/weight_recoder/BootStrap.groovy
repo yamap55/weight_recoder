82ac4a75e9adcd7587359c566956dc8132364ef9
yamap55 <tetuya120@hotmail.com>
2017-03-11 23:26:15 +0900

    [update] test data insert setting

--- grails-app/init/weight_recoder/BootStrap.groovy
+++ grails-app/init/weight_recoder/BootStrap.groovy
@@ -1,8 +1,16 @@
 package weight_recoder
+import grails.util.Environment
 
 class BootStrap {
 
     def init = { servletContext ->
+      environments {
+        development {
+          ["user1","user2","user3"].each {
+            new User(name:it).save()
+          }
+        }
+      }
     }
     def destroy = {
     }
