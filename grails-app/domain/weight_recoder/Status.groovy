e649060ce03f3085707072dfe036cce0fbadb864
yamap55 <tetuya120@hotmail.com>
2017-03-11 23:56:18 +0900

    [add] createDate and updateDate to domain

--- grails-app/domain/weight_recoder/Status.groovy
+++ grails-app/domain/weight_recoder/Status.groovy
@@ -3,7 +3,13 @@ package weight_recoder
 class Status {
   double weight
   double bodyFatPercentage
+  Date dateCreated
+  Date lastUpdated
 
   static constraints = {
+  }
+
+  static mapping = {
+    autoTimestamp true
   }
 }
--- grails-app/domain/weight_recoder/User.groovy
+++ grails-app/domain/weight_recoder/User.groovy
@@ -2,6 +2,13 @@ package weight_recoder
 
 class User {
   String name
+  Date dateCreated
+  Date lastUpdated
+
   static constraints = {
+  }
+
+  static mapping = {
+    autoTimestamp true
   }
 }
